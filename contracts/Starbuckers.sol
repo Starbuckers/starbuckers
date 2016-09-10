557cefbe5f3e15acbc6d909990ed70e621a682e0
Roland Kofler <roland.kofler@gmail.com>
2016-09-10 20:57:36 +0200

    added trade matching

--- contracts/Starbuckers.sol
+++ contracts/Starbuckers.sol
@@ -21,6 +21,8 @@ contract Starbuckers is withEnlistedSecurities{
     }
     
     enum State { PENDING, ACTIVE, REJECTED, CANCELLED }
+    enum BuySell {BUY, SELL}
+    enum TradeState {PENDING, EXECUTED, CANCELLED}
     
     struct Agreement{
         address from;
@@ -30,6 +32,28 @@ contract Starbuckers is withEnlistedSecurities{
         uint16 lendingrate; // in basis points
         State state;
     }
+    
+    struct Order {
+        address from;
+        address to;
+        BuySell buysell;
+        string securitycode;
+        uint16 units;
+        uint32 unitprice;
+    }
+    
+    struct Trade {
+        address buyer;
+        address seller;
+        string securitycode;
+        uint16 units;
+        uint32 unitprice;
+        TradeState state;
+    }
+    
+    Order[] buyOrders;
+    Order[] sellOrders;
+    Trade[] trades;
     
     mapping (address => Account) accounts;
     
@@ -50,6 +74,45 @@ contract Starbuckers is withEnlistedSecurities{
     
     function getAgreementArraySize() constant returns(uint256 size){
         size = agreements.length;
+    }
+    
+    function processOrder(address _from, address _to, uint _buysell, string _securitycode, string _currency, uint16 _units, uint32 _unitprice) {
+        BuySell bs = BuySell(_buysell);
+    
+        if (BuySell.BUY == bs){
+            buyOrders.push( Order (_from, _to, bs, _securitycode, _units, _unitprice));
+        }else{
+            sellOrders.push( Order (_from, _to, bs, _securitycode, _units, _unitprice));
+        }
+    }    
+
+        // if yes: 
+        // delete the matched trade from Trades_pending
+        // insert the trade into Trades_matched
+        // and then call the trade processing function
+        
+        
+        // if not, create hash and append it to Trades_pending
+
+    function matchesOrders(uint256 indexBuy, uint256 indexSell) constant returns (bool matches){
+        Order buy = buyOrders[indexBuy];
+        Order sell = sellOrders[indexSell];
+        if (buy.from != sell.to) return false;
+        if (buy.to != sell.from) return false;
+        var bcode = buy.securitycode.toSlice();
+        var scode = sell.securitycode.toSlice();
+        if (!bcode.equals(scode)) return false;
+        if (buy.units != sell.units) return false;
+        if (buy.unitprice != sell.unitprice) return false;
+        return true;
+    }
+    
+    function makeTrade(uint256 indexBuy, uint256 indexSell){
+        if (!matchesOrders(indexBuy, indexSell)) throw;
+        Order buy = buyOrders[indexBuy];
+        trades.push(Trade(buy.from, buy.to, buy.securitycode, buy.units, buy.unitprice, TradeState.PENDING));
+        delete buyOrders[indexBuy];
+        delete sellOrders[indexSell];
     }
     
     function proposeLendingAgreement(address _to, string _securitycode, uint16 _haircut, uint16 _lendigrate) validSecurityCode(_securitycode) {
